877aaee4faaed83c51d0d3c49c6cfff488443b99
hishamkame <hishamkamel370@gmail.com>
2024-05-01 18:43:22 +0300

    create chat bubble widget

--- lib/screens/chat_page.dart
+++ lib/screens/chat_page.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/widgets/chat_bubble.dart';
 import 'package:chat_app/widgets/my_drawer.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -27,10 +28,10 @@ class ChatPage extends StatelessWidget {
           ],
         ),
         drawer: const MyDrawer(),
-        body: const Center(
-          child: Text(
-            'jdsjdsj',
-          ),
+        body: const Column(
+          children: [
+            ChatBubble(),
+          ],
         ),
       ),
     );
